1555ddb4d6b2cc86695784fb79f43e07805ea20b
Pur <purwanto.p@fitaja.id>
2023-09-12 23:14:23 +0700

    setup

--- src/pages/Onboarding/index.tsx
+++ src/pages/Onboarding/index.tsx
@@ -1,28 +1,66 @@
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import React from 'react';
-import Icon from 'react-native-vector-icons/Entypo';
-import {RootStackParamList} from '../../route';
-import {useNavigation} from '@react-navigation/native';
-import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/EvilIcons';
+import { RootStackParamList } from '../../route';
+import { Colors, Fonts } from '../../utils';
 
 const OnBoarding: React.FC = () => {
   const navigation =
     useNavigation<NativeStackNavigationProp<RootStackParamList>>();
 
-  setTimeout(() => {
-    navigation.navigate('Login');
-  }, 3000);
+  // setTimeout(() => {
+  //   navigation.navigate('Login');
+  // }, 3000);
 
   return (
-    <TouchableOpacity
-      onPress={() => navigation.navigate('Login')}
-      style={{height: '100%', justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{color: 'red'}}>OnBoarding</Text>
-      <Icon name="open-book" size={100} color="red" />
-    </TouchableOpacity>
+    <View>
+      <View style={styles.box1}>
+        <Image source={require('../../assets/images/frame.png')} style={styles.image} />
+      </View>
+      <View style={styles.box2}>
+        <View style={{ marginTop: 64, marginBottom: 15 }}>
+          <Text style={styles.title}>Lorem Ipsum Dolorr</Text>
+        </View>
+        <View style={{ marginBottom: 36, marginHorizontal: '20%' }}>
+          <Text style={styles.desc}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pulvinar hendrerit leo vitae mollis. Cras non volutpat lacus.</Text>
+        </View>
+        <View style={{ alignItems: 'center', marginHorizontal: '20%', flexDirection: 'row', justifyContent: 'space-between' }}>
+          <Text style={styles.back}>Back</Text>
+          <Icon name='arrow-right' size={90} color={Colors.white} />
+        </View>
+      </View>
+    </View>
   );
 };
 
 export default OnBoarding;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: { resizeMode: 'contain', height: '100%', width: '100%' },
+  box1: { height: '40%', width: '100%', marginVertical: 13 },
+  box2: {
+    alignSelf: 'center',
+    height: 605,
+    backgroundColor: Colors.blue,
+    width: 605,
+    borderTopLeftRadius: 390,
+    borderTopRightRadius: 390,
+  },
+  title: {
+    fontFamily: Fonts['700'],
+    fontSize: 30,
+    lineHeight: 38,
+    textAlign: 'center',
+    color: Colors.white,
+  },
+  desc: {
+    fontFamily: Fonts['400'],
+    fontSize: 16,
+    lineHeight: 20,
+    textAlign: 'center',
+    color: Colors.white
+  },
+  back: { fontFamily: Fonts['500'], color: Colors.white, fontSize: 16, lineHeight: 21 }
+});
