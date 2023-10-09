bf9d1d65450ca82bdafcb09f687a51049b429f43
mahrus-rohisyam <mahrusaim@gmail.comm>
2023-10-09 18:06:17 +0700

    fix: SVG not rendered and assets won't imported

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,21 +1,31 @@
-import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView } from 'react-native'
-import React from 'react'
-import { useNavigation } from '@react-navigation/native'
-import { NativeStackNavigationProp } from '@react-navigation/native-stack'
-import { RootStackParamList } from '../../route'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+} from 'react-native';
+import React from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../../route';
+import {SvgUri} from 'react-native-svg';
+import Group from '../../assets/images/group.svg';
 
-type Props = {}
+type Props = {};
 
 const Home = (props: Props) => {
-  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>();
 
   return (
     <SafeAreaView style={{backgroundColor: 'pink', flex: 1}}>
       <Text>HOME</Text>
+      <Group width={300} height={300} />
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
